90da70ac58eba13b38e0093b34b2d3ac00c7e803
Azim Munawwar Gani Khairdi <58516056+mdazim764@users.noreply.github.com>
2026-08-31 22:33:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,15 +8,16 @@ require("dotenv").config();
 
 // 2. INITIALIZING APP & MIDDLEWARE
 const app = express();
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "https://react-portfolio-cv-git-main-azim-khairdis-projects.vercel.app",
-    ], // Replace with your actual Vercel URL!
-    credentials: true,
-  }),
-);
+// app.use(
+//   cors({
+//     origin: [
+//       "http://localhost:5173",
+//       "https://react-portfolio-cv-git-main-azim-khairdis-projects.vercel.app",
+//     ], // Replace with your actual Vercel URL!
+//     credentials: true,
+//   }),
+// );
+app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
